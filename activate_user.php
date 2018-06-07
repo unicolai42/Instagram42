c87ce5b68092fb164317f513f35c903dcbc93e83
UgoNic <ugo.nicolai@gmail.com>
2018-06-07 20:35:59 +0200

    protect activate user against fake key

--- activate_user.php
+++ activate_user.php
@@ -1,8 +1,30 @@
 <?PHP
     session_start();
 
+    if (empty($_GET['cle'])) {
+        header("Location: connexion.php");
+        exit();
+    }
+
     include_once 'config/database.php';
     $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false));
+    
+    $sql = "SELECT cle FROM users;";
+    $sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY)); 
+    $sth->execute();
+    $cles = $sth->fetch();
+
+    $cle_exist = 0;
+
+    foreach($cles as $cle)
+        if ($cle == $_GET['cle'])
+            $cle_exist = 1;
+    
+    if ($cle_exist == 0) {
+        header("Location: connexion.php");
+        exit();
+    }
+
     
     $sql = "UPDATE users SET activate = 1 WHERE cle = :cle;";
     $sth = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY)); 
